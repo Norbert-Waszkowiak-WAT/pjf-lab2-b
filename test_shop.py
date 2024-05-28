77b87726dbae7c6ef729fc9a453d27406ca49d98
Norbert Waszkowiak <norbert.waszkowiak@gmail.com>
2024-05-28 09:16:50 +0200

    update test

--- test_shop.py
+++ test_shop.py
@@ -15,11 +15,11 @@ class TestShop(unittest.TestCase):
 
     def test_add_product(self):
         self.supplier.deliver_product(self.notebook)
-        self.assertIn(self.notebook, self.shop.products)
+        self.assertIn(self.notebook, self.shop.paper_products)
 
     def test_add_product_again(self):
         self.supplier.deliver_product(self.bread)
-        self.assertIn(self.bread, self.shop.products)
+        self.assertIn(self.bread, self.shop.food_products)
 
     def test_check_availability(self):
         self.supplier.deliver_product(self.pen)
@@ -31,20 +31,20 @@ class TestShop(unittest.TestCase):
     def test_sell_food_product(self):
         self.supplier.deliver_product(self.bread)
         self.shop.sell_food_product("Bread")
-        self.assertNotIn(self.bread, self.shop.products)
+        self.assertNotIn(self.bread, self.shop.food_products)
 
     def test_sell_food_product_again(self):
         self.shop.sell_food_product("NotExistingProduct")
-        self.assertNotIn("NotExistingProduct", [product.name for product in self.shop.products])
+        self.assertNotIn("NotExistingProduct", [product.name for product in self.shop.food_products])
 
     def test_sell_paper_product(self):
         self.supplier.deliver_product(self.notebook)
         self.shop.sell_paper_product("Notebook")
-        self.assertNotIn(self.notebook, self.shop.products)
+        self.assertNotIn(self.notebook, self.shop.paper_products)
 
     def test_sell_paper_product_again(self):
         self.shop.sell_paper_product("NotExistingProduct")
-        self.assertNotIn("NotExistingProduct", [product.name for product in self.shop.products])
+        self.assertNotIn("NotExistingProduct", [product.name for product in self.shop.paper_products])
 
     def test_print_product_list(self):
         self.supplier.deliver_product(self.notebook)
